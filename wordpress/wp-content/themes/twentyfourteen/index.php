63cfd6baf555f6cec85c615a8f4d00736a85b1b8
Ben Marten <ben.marten@me.com>
2016-05-30 15:55:21 -0700

    implemented retina image

--- wordpress/wp-content/themes/twentyfourteen/index.php
+++ wordpress/wp-content/themes/twentyfourteen/index.php
@@ -28,28 +28,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
 
-		<?php
-			if ( have_posts() ) :
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
-
-					/*
-					 * Include the post format-specific template for the content. If you want to
-					 * use this in a child theme, then include a file called called content-___.php
-					 * (where ___ is the post format) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-
-				endwhile;
-				// Previous/next post navigation.
-				twentyfourteen_paging_nav();
-
-			else :
-				// If no content, include the "No posts found" template.
-				get_template_part( 'content', 'none' );
-
-			endif;
-		?>
+      <img src="img/personalization.png" srcset="img/personalization.png 1x, img/personalization@2x.png 2x, img/personalization@3x.png 3x, img/personalization@4x.png 4x" width="888" height="594" alt="personalization">
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
